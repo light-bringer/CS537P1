5010493011e96ea4714165e3d711c53327967750
Peyton Gardipee <peyton@rockhopper-04.cs.wisc.edu>
2018-09-24 19:47:07 -0500

    Added struct in main.h and made process available to main.c

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef main_h
+#define main_h
+
+typedef struct flag{
+        int p;
+        int pid;
+        int s;
+        int U;
+        int S;
+        int v;
+        int c;
+} flag;
+
+#endif
